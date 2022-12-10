b619cdc0e697723ea71047cd11a4d2bbad2a3105
Matt Ettus <matt@ettus.net>
2022-12-10 09:50:00 -0800

    solves 10-a

---
+++ advent10.jl
@@ -0,0 +1,36 @@
+
+lines = readlines("advent10.test")
+lines = readlines("advent10.input")
+
+cycle = 1
+v = 1
+stored_v = []
+phase = 0
+pc = 0
+delta_v = 0
+
+while cycle <= 220
+    # if (cycle - 20) % 40 == 0
+    #     push!(stored_v, v*cycle)
+    # end
+    if phase == 0
+        pc += 1
+        v += delta_v
+        if lines[pc][1:4] == "addx"
+            delta_v = parse(Int, lines[pc][6:end])
+            phase = 1
+        else
+            delta_v = 0
+        end
+    else
+        phase = 0  # noop
+    end
+    if (cycle - 20) % 40 == 0
+        push!(stored_v, v*cycle)
+    end
+    println(cycle,":\tV:",v,"\tPC: ", pc, "\tPHASE: ", phase,"\tINS: ", lines[pc])
+    cycle += 1
+end
+
+stored_v
+sum(stored_v)
