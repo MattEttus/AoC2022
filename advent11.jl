cf4e37e9bf30ce0f6461128523917241167834ea
Matt Ettus <matt@ettus.net>
2022-12-11 11:31:46 -0800

    solves first part of advent 11

--- advent11.jl
+++ advent11.jl
@@ -1,7 +1,4 @@
 
-file = open("advent11.test")
-file = open("advent11.input")
-
 mutable struct Monkey
     number::Int
     items::Array{Int}
@@ -18,7 +15,13 @@ function Monkey(fp)
     number = parse(Int, split(line," ")[2][1:end-1])
     println("Processing monkey #", number)
     items = eval(Meta.parse("["*split(readline(fp),":")[2]*"]"))
-    operation = Meta.parse(split(readline(fp),":")[2])
+    
+    #operation = Meta.parse(split(readline(fp),":")[2])
+    fn_def = "fn$number(old) = " * split(readline(fp),"=")[2]
+    println("operation will be: ", fn_def)
+    operation = eval(Meta.parse(fn_def))
+    # operation = eval(Meta.parse("fn(old) = " * split(readline(fp),"=")[2]))
+    
     factor = parse(Int,split(readline(fp),"by")[2])
     
     line1 = split(readline(file)," ",keepempty=false)
@@ -34,6 +37,23 @@ function Monkey(fp)
     Monkey(number,items,operation,test, 0)
 end
 
+function round(monkeys, num_monkeys)
+    for num in 0:num_monkeys-1
+        monkey = monkeys[num]
+        while ~isempty(monkey.items)
+            monkey.inspections += 1
+            item = popfirst!(monkey.items)
+            println("Item: ", item)
+            newitem = monkey.operation(item) ÷ 3
+            println("Newitem: ", newitem)
+            push!(monkeys[monkey.test(newitem)].items,newitem)
+        end
+    end
+end
+
+file = open("advent11.test")
+file = open("advent11.input")
+
 monkeys = Dict()
 while ~eof(file)
     monkey = Monkey(file)
@@ -42,21 +62,18 @@ end
 
 num_monkeys = length(monkeys)
 
-# Do a round
-function round(monkeys, num_monkeys)
-    for num in 0:num_monkeys-1
-        monkey = monkeys[num]
-        while ~isempty(monkey.items)
-            old = popfirst!(monkey.items)
-            println(old)
-            push!(monkeys[monkey.test(eval(monkey.operation))],new)
-            # push!(monkeys[monkey.test(eval(monkey.operation))],new)
-        end
-    end
-end
-
-round(monkeys, num_monkeys)
-
 for i in 0:num_monkeys-1
     println(i,": ", monkeys[i].items)
 end
+
+for i in 1:20
+    round(monkeys, num_monkeys)
+end
+
+insp = []
+for i in 0:num_monkeys-1
+    println(i,": ", monkeys[i].inspections,"\t",monkeys[i].items)
+    push!(insp,monkeys[i].inspections)
+end
+
+reduce(*, sort(insp,rev=true)[1:2])
