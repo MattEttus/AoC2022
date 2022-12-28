e276664df0a4cb599ef65ddc306561fe46302b12
Matt Ettus <matt@ettus.net>
2022-12-28 13:50:23 -0800

    lots of changes for advent16 to do intelligent pathfinding instead of random walks

--- advent16.jl
+++ advent16.jl
@@ -1,8 +1,61 @@
+# advent16.jl
+
+# Routing algorithms
+function make_dist_map(tunnels)
+    dist = fill(10000,length(tunnels),length(tunnels))
+    for (src,dsts) in enumerate(tunnels)
+        for dst in dsts
+            dist[src,dst] = 1
+        end
+    end
+    for i in 1:length(tunnels)
+        dist[i,i] = 0
+    end
+    flag = true
+    while flag
+        flag = false
+        for src in 1:length(tunnels)
+            for dst in 1:length(tunnels)
+                for via in 1:length(tunnels)
+                    if dist[src,via] + dist[via,dst] < dist[src,dst]
+                        dist[src,dst] = dist[src,via] + dist[via,dst]
+                        flag = true
+                    end
+                end
+            end
+        end
+    end
+    dist
+end
+
+function route_step(src,dst,dist)
+    best = 0
+    min_dist = 1000
+    for next in 1:size(dist)[1]
+        if dist[src,next] == 1
+            if dist[next,dst] < min_dist
+                min_dist = dist[next,dst]
+                best = next
+            end
+        end
+    end
+    best
+end
+
+function print_route(src,dst,dist)
+    while src != dst
+        src = route_step(src,dst,dist)
+        print(src, ", ")
+    end
+end
+
 # State is valve states, completed flow, current location.  Time is implicit
 struct State
     flow::Int
     loc::Int
+    goal::Int
     loc_elephant::Int
+    goal_elephant::Int
     valves::Vector{Bool}
 end
 
@@ -27,11 +80,10 @@ function load_map(filename)
     for (num, line) in enumerate(readlines(filename))
         tokens = split(line," ")
         names[tokens[2]] = num
-        # println(parse(Int,tokens[5][6:end-1]))
         push!(flow_rates,parse(Int,tokens[5][6:end-1]))
         push!(init_valves,flow_rates[end] == 0)
     end
-    Initial_State = State(0,names["AA"],names["AA"],init_valves)
+    Initial_State = State(0,names["AA"],0,names["AA"],0,init_valves)
 
     # 2nd pass to make map
     for line in readlines(filename)
@@ -41,39 +93,50 @@ function load_map(filename)
     return names, flow_rates, Initial_State, tunnels
 end
 
-function gen_moves(state,flow_rates,tunnels) #::State, flow_rates, tunnels)
-    moves = [] # Vector{State}[]
+function gen_moves(state,flow_rates,dist) #::State, flow_rates, tunnels)
     flow = state.flow + sum(state.valves .* flow_rates)
     if all_on(state)
-        return [State(flow,state.loc,state.loc_elephant,state.valves)]
-    end
-    for el_move in gen_moves2(state,flow_rates,tunnels)
-        # First try turning on a valve
-        if ~el_move.valves[state.loc]
-            new_valves=copy(el_move.valves)
-            new_valves[state.loc] = true
-            push!(moves,State(flow,state.loc,el_move.loc_elephant,new_valves))
+        [State(flow,state.loc,0,state.loc_elephant,0,state.valves)]
+    else
+        moves = []
+        for el_move in gen_moves2(state,dist)
+            if all_on(el_move)
+                push!(moves,State(flow,el_move.loc,0,el_move.loc_elephant,0,el_move.valves))
+            elseif el_move.goal == 0 || el_move.valves[el_move.goal]
+                # Either goal accomplished or other guy got there first  
+                append!(moves,[State(flow,route_step(el_move.loc,goal,dist),goal,el_move.loc_elephant,el_move.goal_elephant,el_move.valves) for goal in findall(y -> ~y, el_move.valves)])
+            elseif el_move.goal == el_move.loc
+                # Arrived at our goal, open the valve
+                new_valves=copy(el_move.valves)
+                new_valves[el_move.goal] = true
+                push!(moves,State(flow,el_move.loc,0,el_move.loc_elephant,el_move.goal_elephant,new_valves))
+            else
+                push!(moves,State(flow,route_step(el_move.loc,el_move.goal,dist),el_move.goal,el_move.loc_elephant,el_move.goal_elephant,el_move.valves))
+            end
         end
-        for tunnel in tunnels[state.loc]
-            push!(moves,State(flow,tunnel,el_move.loc_elephant,el_move.valves))
-        end
+        moves
     end
-    moves
 end
 
-function gen_moves2(state,flow_rates,tunnels) #::State, flow_rates, tunnels)
-    moves = [] # Vector{State}[]
-    
-    # First try turning on a valve
-    if ~state.valves[state.loc_elephant]
+function gen_moves2(state,dist) #::State, flow_rates, tunnels)
+    if state.goal_elephant == 0 || state.valves[state.goal_elephant]
+        # Either goal accomplished or other guy got there first
+        if all_on(state)
+            # All valves open, just stay still
+            [State(state.flow,state.loc,state.goal,state.loc_elephant,0,state.valves)]
+        else
+            # generate a move towards each valve which is still off
+            [State(state.flow,state.loc,state.goal,route_step(state.loc_elephant,goal,dist),goal,state.valves) for goal in findall(y -> ~y, state.valves)]
+        end
+    elseif state.goal_elephant == state.loc_elephant
+        # Arrived at goal, let's open the valve
         new_valves=copy(state.valves)
-        new_valves[state.loc_elephant] = true
-        push!(moves,State(state.flow,state.loc,state.loc_elephant,new_valves))
+        new_valves[state.goal_elephant] = true
+        [State(state.flow,state.loc,state.goal,state.loc_elephant,0,new_valves)]
+    else
+        # Keep going towards goal
+        [State(state.flow,state.loc,state.goal,route_step(state.loc_elephant,state.goal_elephant,dist), state.goal_elephant,state.valves)]
     end
-    for tunnel in tunnels[state.loc_elephant]
-        push!(moves,State(state.flow,state.loc,tunnel,state.valves))
-    end
-    moves
 end
 
 function eval_moves(moves,states) #::Vector{State}, states::Vector{State})
@@ -120,18 +183,8 @@ function max_flow(states)
 end
 
 names, flow_rates, Initial_State, tunnels = load_map(("advent16.test","advent16.input")[2])
+
 cur_states = [Initial_State]
-
-# for cycle in 1:26
-#     next_states = []
-#     for cur_state in cur_states
-#         moves = gen_moves(cur_state, flow_rates, tunnels)
-#         next_states = eval_moves2(moves, next_states)
-#     end
-#     cur_states = next_states
-#     println("Cycle $cycle: Num states: $(length(cur_states))  Best Flow: $(max_flow(cur_states))")
-# end
-
 for cycle in 1:26
     moves = []
     for cur_state in cur_states
