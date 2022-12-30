4ef737ad4c64994b955b052372976adb4ba82955
Matt Ettus <matt@ettus.net>
2022-12-30 09:18:04 -0800

    advent22: cleanup of part 1

--- advent22.jl
+++ advent22.jl
@@ -10,83 +10,83 @@ function wrap(pos,dir,lim)
     end
 end
 
-filename = ("advent22.test","advent22.input")[2]
+function read_input(filename)
+    points = Dict{CartesianIndex,Bool}()
+    rows = 0
+    columns = 0
 
-points = Dict{CartesianIndex,Bool}()
-
-rows = 0
-columns = 0
-
-for (row,line) in enumerate(readlines(filename))
-    if length(line) == 0
-        rows = row-1
-        break
-    end
-    columns = max(columns, length(line))
-    for (column,char) in enumerate(line)
-        if char == '#'
-            points[CartesianIndex(row,column)] = true
-        elseif char == '.'
-            points[CartesianIndex(row,column)] = false
+    for (row,line) in enumerate(readlines(filename))
+        if length(line) == 0
+            rows = row-1
+            break
+        end
+        columns = max(columns, length(line))
+        for (column,char) in enumerate(line)
+            if char == '#'
+                points[CartesianIndex(row,column)] = true
+            elseif char == '.'
+                points[CartesianIndex(row,column)] = false
+            end
         end
     end
-end
-lim = CartesianIndex(rows,columns)
-commands = readlines(filename)[rows+2]
-# right, down, left, up
-move = Dict([(0,CartesianIndex(0,1)),(1,CartesianIndex(1,0)),(2,CartesianIndex(0,-1)),(3,CartesianIndex(-1,0))]);
-
-pos = CartesianIndex(1,1);
-while ~(pos in keys(points)) || points[pos] == true
-    pos += CartesianIndex(0,1)
+    lim = CartesianIndex(rows,columns);
+    commands = readlines(filename)[rows+2];
+    return lim, commands, points
 end
 
-dir = 0;
-num_start = 1;
-num_finish = 1;
+function run_commands(lim,commands,points)
+    # right, down, left, up
+    move = Dict([(0,CartesianIndex(0,1)),(1,CartesianIndex(1,0)),(2,CartesianIndex(0,-1)),(3,CartesianIndex(-1,0))]);
 
-while num_finish <= length(commands)
-    while num_finish < length(commands) && commands[num_finish+1] in "0123456789"
-        num_finish +=1
+    pos = CartesianIndex(1,1);
+    while ~(pos in keys(points)) || points[pos] == true
+        pos += CartesianIndex(0,1)
     end
-    count = parse(Int,commands[num_start:num_finish])
 
-    println(pos)
-    println("Dir:", dir," ", count)
+    dir = 0;
+    num_start = 1;
+    num_finish = 1;
 
-    for i in 1:count
-        if pos+move[dir] in keys(points)
-            if points[pos+move[dir]]
-                break
+    while num_finish <= length(commands)
+        while num_finish < length(commands) && commands[num_finish+1] in "0123456789"
+            num_finish +=1
+        end
+        count = parse(Int,commands[num_start:num_finish])
+        for i in 1:count
+            if pos+move[dir] in keys(points)
+                if points[pos+move[dir]]
+                    break
+                else
+                    pos += move[dir]
+                end
             else
-                pos += move[dir]
+                new_p = wrap(pos,dir,lim)
+                # print("wrapped: ",new_p)
+                while new_p ∉ keys(points)
+                    new_p += move[dir]
+                end
+                if points[new_p] == false
+                    pos = new_p
+                else
+                    break
+                end
             end
+        end
+        if num_finish == length(commands)
+            break
+        end
+        if commands[num_finish+1] == 'R'
+            dir = (dir + 1) % 4
         else
-            println("Here")
-            new_p = wrap(pos,dir,lim)
-            print("wrapped: ",new_p)
-            while new_p ∉ keys(points)
-                new_p += move[dir]
-            end
-            if points[new_p] == false
-                pos = new_p
-            else
-                break
-            end
+            dir = (dir + 3) % 4
         end
+        num_start = num_finish + 2
+        num_finish = num_start
     end
-
-    if num_finish == length(commands)
-        break
-    end
-    if commands[num_finish+1] == 'R'
-        dir = (dir + 1) % 4
-    else
-        dir = (dir + 3) % 4
-    end
-    println(commands[num_finish+1])
-    num_start = num_finish + 2
-    num_finish = num_start
+    return pos,dir
 end
 
+filename = ("advent22.test","advent22.input")[2]
+(lim, commands, points) = read_input(filename);
+(pos, dir) = run_commands(lim, commands, points)
 println("Part 1: $(1000*pos[1]+4*pos[2]+dir)")
